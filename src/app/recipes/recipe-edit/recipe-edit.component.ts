f5204a71024bc4a6cad3b2311dfe0b3275d8a2b4
Yariki <iyariki.ya@gmail.com>
2018-02-01 00:47:06 +0200

    initial implementation of editing recipe with reactive approach

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {checkEdit} from 'tslint';
+import {FormControl, FormGroup, NgForm} from '@angular/forms';
+import {RecipeService} from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,8 +13,10 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
 
@@ -20,10 +24,35 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
-
+        this.initForm();
       }
     );
+  }
 
+  private initForm() {
+
+    let recipeName = '';
+    let recipeImgUrl = '';
+    let recipeDescription = '';
+
+    if ( this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImgUrl = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath' : new FormControl(recipeImgUrl),
+      'description' : new FormControl(recipeDescription)
+    });
+
+
+
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
